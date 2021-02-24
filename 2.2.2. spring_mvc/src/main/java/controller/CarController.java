7bdf9993b126e3117d12f6f0fb5f4f8bdab0861b
Iliya333 <69747853+Iliya333@users.noreply.github.com>
2021-02-24 21:17:54 +0500

    Поправил недочеты 2

--- 2.2.2. spring_mvc/src/main/java/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/controller/CarController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import model.Car;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,21 +13,9 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    public CarController() {
-    }
-
-    private ServiceCar serviceCar;
-
-    @Autowired
-    public CarController(ServiceCar serviceCar) {
-        this.serviceCar = serviceCar;
-    }
-
     @GetMapping(value = "/cars")
     public String getCars(@RequestParam(value = "count", defaultValue = "0") int count, Model model) {
-        if (count == 0){
-            count = serviceCar.getSize();
-        }
+        ServiceCar serviceCar = new ServiceCar();
         List<Car> carList = serviceCar.getCarList(count);
         model.addAttribute("cars", carList);
         return "cars";
--- 2.2.2. spring_mvc/src/main/java/service/ServiceCar.java
+++ 2.2.2. spring_mvc/src/main/java/service/ServiceCar.java
@@ -3,7 +3,6 @@ package service;
 import model.Car;
 import org.springframework.stereotype.Service;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,13 +21,8 @@ public class ServiceCar {
 
     }
 
-
     public List<Car> getCarList(int count) {
         return carList.stream().limit(count).collect(Collectors.toList());
-    }
-
-    public int getSize() {
-        return carList.size();
     }
 
 }
